d119b7426fb64502edcba1fe782f4c9e7cf09c38
Ahmed Bodiwala <ahmed_bodi@msn.com>
2024-09-01 02:10:55 +0100

    refactor: Refactor Android Meraki Recipe

--- contrib/android/p4a_recipes/meraki/__init__.py
+++ contrib/android/p4a_recipes/meraki/__init__.py
@@ -1,8 +1,15 @@
-from pythonforandroid.recipe import CythonRecipe
+from pythonforandroid.recipe import CompiledComponentsPythonRecipe
 
-class MerakiRecipe(CythonRecipe):
+class MerakiRecipe(CompiledComponentsPythonRecipe):
     version = "0.5.2"
     url = 'https://github.com/Telestai-Project/meraki/archive/{version}.zip'
-    depends = ['setuptools']
+    site_packages_name = 'meraki'
+    depends = ['openssl', 'six', 'setuptools', 'cffi', 'python3', 'shared', 'pycparser', 'libffi']
+    call_hostpython_via_targetpython = False
+    need_stl_shared = True
+
+    def should_build(self, arch):
+        return True
+
 
 recipe = MerakiRecipe()
