c60c4df0b3242f32f5519121efdf8f7ea8d4aac9
Ahmed Bodiwala <ahmed_bodi@msn.com>
2024-09-14 14:53:31 -0400

    Update AlgoModule in Android Recipe and revert CI Changes

--- contrib/android/p4a_recipes/algomodule/__init__.py
+++ contrib/android/p4a_recipes/algomodule/__init__.py
@@ -1,7 +1,7 @@
 from pythonforandroid.recipe import CythonRecipe
 
 class AlgoModuleRecipe(CythonRecipe):
-    version = "1.1.2"
+    version = "1.1.3"
     url = 'https://github.com/electrum-altcoin/AlgoLib/archive/{version}.zip'
     depends = ['setuptools']
 
